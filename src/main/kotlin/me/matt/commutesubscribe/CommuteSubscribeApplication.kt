f639f9dc6531c0b0519a32bf08bbb60b5b0f2bb9
51f0x <51f0x>
2021-09-25 14:46:46 +0200

    Initial commit

---
+++ src/main/kotlin/me/matt/commutesubscribe/CommuteSubscribeApplication.kt
@@ -0,0 +1,12 @@
+package me.matt.commutesubscribe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommuteSubscribeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommuteSubscribeApplication>(*args)
+}
+
---
+++ src/test/kotlin/me/matt/commutesubscribe/CommuteSubscribeApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.matt.commutesubscribe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommuteSubscribeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
