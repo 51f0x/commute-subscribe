0db142ebed5e007b34b1192791592c54e37b7a91
51f0x <51f0x>
2021-09-26 16:58:21 +0200

    implement gradle jib

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("com.google.cloud.tools.jib") version "3.1.4"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
 
@@ -11,36 +10,6 @@ plugins {
 
 group = "me.matt"
 version = "0.0.1-SNAPSHOT"
-
-jib {
-    from {
-        image = "adoptopenjdk:11-jre-hotspot"
-    }
-    to {
-        image = "commute-subscribe:latest"
-    }
-    container {
-        entrypoint = listOf("bash", "-c", "/entrypoint.sh")
-        ports = listOf("8081")
-        environment = mapOf(
-            "SPRING_OUTPUT_ANSI_ENABLED" to "ALWAYS",
-            "JHIPSTER_SLEEP" to "0"
-        )
-        creationTime = "USE_CURRENT_TIMESTAMP"
-        user = "1000"
-    }
-    extraDirectories {
-        permissions = mapOf(
-            "/entrypoint.sh" to "755"
-        )
-
-        paths {
-            "src/main/docker/jib"
-        }
-
-    }
-}
-
 
 repositories {
     mavenCentral()
